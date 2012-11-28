bda911ca96a54a81b7e983ca1759f70ac43df927
Adam Kress <akress@akress.com>
2012-11-28 06:57:12 -0500

    getting somewhere

--- www/js/common.js
+++ www/js/common.js
@@ -1,3 +1,9 @@
-$(document).ready(function(){
-    //alert("jquery is working");
+$("div[data-role*='page']").live('pageshow', function(event, ui) { 
+    $.mobile.allowCrossDomainPages = true;
+    $(".search_btn").bind('tap',function(){
+        $("#device_result").html("<h1>Please hold..</h1><h2>"+$("#serial_search").val()+"</h2>");
+        $.getJSON("http://akress.com/test.php",{sa_test: true, action: "serial_search", search: $("#serial_search").val()}, function(data){
+            $("#device_result").html("<h1>"+data.serial+"</h1><h2>"+data.asset+"</h2>");    
+        });
+    });
 });
