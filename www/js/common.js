e442c0a26a52c2483582d51d69ee026510d38a54
Adam Kress <akress@akress.com>
2012-11-29 01:25:19 -0500

    json returns..

--- www/js/common.js
+++ www/js/common.js
@@ -1,9 +1,12 @@
 $("div[data-role*='page']").live('pageshow', function(event, ui) { 
-    $.mobile.allowCrossDomainPages = true;
-    $(".search_btn").bind('tap',function(){
+    $.mobile.allowCrossDomainPages = true;    
+    $(".search_btn").bind('tap',function(){     
         $("#device_result").html("<h1>Please hold..</h1><h2>"+$("#serial_search").val()+"</h2>");
-        $.getJSON("http://akress.com/test.php",{sa_test: true, action: "serial_search", search: $("#serial_search").val()}, function(data){
-            $("#device_result").html("<h1>"+data.serial+"</h1><h2>"+data.asset+"</h2>");    
-        });
+        $.post("http://akress.com/test.php",{ actn: "serial_search", lukeup: $("#serial_search").val() }, 
+            function(data){
+                $("#device_result").html("<h1>"+data.serial+"</h1><h2>"+data.asset+"</h2>");    
+        },'json');
     });
+    
+    
 });
