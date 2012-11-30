a55592c23c968ea2e149b839c91c88cabab2e8d6
Adam Kress <akress@akress.com>
2012-11-30 00:09:21 -0500

    soma displayo

--- www/js/common.js
+++ www/js/common.js
@@ -4,7 +4,10 @@ $("div[data-role*='page']").live('pageshow', function(event, ui) {
         $("#device_result").html("<h1>Please hold..</h1><h2>"+$("#serial_search").val()+"</h2>");
         $.post("http://akress.com/test.php",{ actn: "serial_search", lukeup: $("#serial_search").val() }, 
             function(data){
-                $("#device_result").html("<h1>"+data.serial+"</h1><h2>"+data.asset+"</h2>");    
+                var html_str = "<label>Serial#: <span>"+data.serial+"</span></label>";
+                html_str += "<label>Asset: <span>"+data.serial+"</span></label>";
+                html_str += "<label>Device type: <span>"+data.devtype+"</span></label>";
+                $("#device_result").html(html_str);    
         },'json');
     });
     
